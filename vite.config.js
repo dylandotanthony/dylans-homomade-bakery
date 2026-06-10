ebe9e677238c1832456b054899881512efa33c08
Dylan Daughenbaugh <99678496+dylandotanthony@users.noreply.github.com>
2026-06-10 12:29:04 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// More details can be found in [Vite's configuration guide](https://vite.dev/config/)
 export default defineConfig({
   plugins: [react()],
+  base: "/", // <-- ADD THIS LINE: Tells Vite to serve assets from the root domain
 })
