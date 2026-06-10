66302d64af7ffb0f94888256bc52785ce78e746f
Dylan Daughenbaugh <99678496+dylandotanthony@users.noreply.github.com>
2026-06-10 12:36:30 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom'; // Changed to HashRouter
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 
-// Import your page components
 import Home from './Home';
 import Nutrition from './Nutrition';
 import Calculator from './Calculator';
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
-        {/* The root URL shows the bakery landing page */}
         <Route path="/" element={<Home />} />
-        
-        {/* These define your two new routes */}
         <Route path="/nutrition" element={<Nutrition />} />
         <Route path="/calculator" element={<Calculator />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
