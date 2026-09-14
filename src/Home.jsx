db4bc2914c13f8a6189eb16c53ae1da4cf8f5e4d
Dylan Daughenbaugh <99678496+dylandotanthony@users.noreply.github.com>
2026-09-13 18:39:05 -0600

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,6 +6,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 // Import all images directly from src/assets/
 import bannerImg from './assets/banner.png';
+import ddhg from './assets/ddhg.png';
 import logoImg from './assets/dlogo.png';
 import facebookIcon from './assets/facebook.svg';
 import instagramIcon from './assets/instagram.svg';
